f1202c996974fc6d4cd86ce24bf149e35d3de0bb
indiacounselling1 <indiacounselling@outlook.com>
2025-07-22 15:41:01 +0530

    Create auth.ts

---
+++ app/lib/auth.ts
@@ -0,0 +1,13 @@
+import GithubProvider from "next-auth/providers/github";
+
+const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    })
+  ],
+  // ...any other config
+};
+
+export default authOptions;
