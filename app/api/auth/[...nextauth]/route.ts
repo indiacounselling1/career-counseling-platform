4f532f67b875209931879dd0d50f484f41eb291c
indiacounselling1 <indiacounselling@outlook.com>
2025-07-22 19:34:03 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,14 @@
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
+import GithubProvider from "next-auth/providers/github";
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
 export { handler as GET, handler as POST };
